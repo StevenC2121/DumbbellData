cd5f1a93399125178e8d163ddd94816200e8ab61
StevenC2121 <stevencabral2121@gmail.com>
2023-06-20 12:30:33 -0400

    Connected to MongoDB
    Application is now connected to the MongoDB database and is ready to use.

--- server/server.js
+++ server/server.js
@@ -1,19 +1,25 @@
-const express = require('express');
+const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 const cors = require("cors");
-require('dotenv').config();
-const port = process.env.port || 5000;
-
+require("dotenv").config({ path: "./config.env" });
+const port = 5000;
 app.use(cors());
-app.use(express.json());
-app.use(require("./routes/record"));
 
-const dbo = require("./db/conn");
+const uri = process.env.ATLAS_URI;
+
+async function connect() {
+    try{
+        await mongoose.connect(uri);
+        console.log("Connected to MongoDb")
+    }
+    catch(error){
+        console.error(error);
+    }
+}
+connect();
 
 app.listen(port, () => {
-    dbo.connectToServer(function (err) {
-        if(err) console.error(err);
-    });
-    console.log(`Listening on port: ${port}`);
+    console.log(`Server started on port: ${port}`);
 });
 
