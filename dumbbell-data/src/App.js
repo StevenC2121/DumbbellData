43f7b07016d3ac305bdca19701ba8175263e13db
StevenC2121 <stevencabral2121@gmail.com>
2023-06-27 12:21:25 -0400

    Updated navbar / naming updates
    Centers the navbar more into the middle of the screen for a better look on the site. Also changed the 'components' folder to 'pages' so it makes more sense when looking at the code.

--- dumbbell-data/src/App.js
+++ dumbbell-data/src/App.js
@@ -1,26 +1,30 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import NavigationBar from './navbar';
-import Home from './components/home';
-import Cardio from './components/cardio';
-import Weightlifting from './components/weightlifting';
-import WLstats from './components/stats-weightlifting';
-import CardioStats from './components/stats-cardio';
+import Home from './pages/home';
+import Cardio from './pages/cardio';
+import Weightlifting from './pages/weightlifting';
+import WLstats from './pages/stats-weightlifting';
+import CardioStats from './pages/stats-cardio';
 
 function App() {
   return (
-    <>
-      <NavigationBar />
-      <div className="container">
-        <Routes>
-          <Route path = "/" element={<Home />} />
-          <Route path="/weightlifting" element={<Weightlifting />} />
-          <Route path="/cardio" element={<Cardio />} />
-          <Route path="/wl-stats" element={<WLstats />} />
-          <Route path="/cardio-stats" element={<CardioStats />} />
-        </Routes>
+    <div style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
+      <div style={{ flex: 1 }}>
+        <NavigationBar />
       </div>
-    </>
+      <div style={{ flex: 1, display: 'flex', justifyContent: 'center' }}>
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/weightlifting" element={<Weightlifting />} />
+            <Route path="/cardio" element={<Cardio />} />
+            <Route path="/wl-stats" element={<WLstats />} />
+            <Route path="/cardio-stats" element={<CardioStats />} />
+          </Routes>
+        </div>
+      </div>
+    </div>
   );
 }
 
--- dumbbell-data/src/navbar.js
+++ dumbbell-data/src/navbar.js
@@ -9,13 +9,15 @@ const NavigationBar = () => {
       <Navbar.Brand as={NavLink} to="/">Dumbbell Data</Navbar.Brand>
       <Navbar.Toggle aria-controls="navbarNavDropdown" />
       <Navbar.Collapse id="navbarNavDropdown">
-        <Nav className="ml-auto">
-          <Nav.Link as={NavLink} to="/weightlifting" exact activeClassName="active">Weightlifting</Nav.Link>
-          <Nav.Link as={NavLink} to="/cardio" activeClassName="active">Cardio</Nav.Link>
-          <NavDropdown title="Graphs/Stats" id="navbarDropdownMenuLink">
-            <NavDropdown.Item as={NavLink} to="/wl-stats" activeClassName="active">Weightlifting</NavDropdown.Item>
-            <NavDropdown.Item as={NavLink} to="/cardio-stats" activeClassName="active">Cardio</NavDropdown.Item>
-          </NavDropdown>
+        <Nav className="mx-auto">
+          <div className="d-flex align-items-center justify-content-center">
+            <Nav.Link as={NavLink} to="/weightlifting" exact activeClassName="active">Weightlifting</Nav.Link>
+            <Nav.Link as={NavLink} to="/cardio" activeClassName="active">Cardio</Nav.Link>
+            <NavDropdown title="Graphs/Stats" id="navbarDropdownMenuLink">
+              <NavDropdown.Item as={NavLink} to="/wl-stats" activeClassName="active">Weightlifting</NavDropdown.Item>
+              <NavDropdown.Item as={NavLink} to="/cardio-stats" activeClassName="active">Cardio</NavDropdown.Item>
+            </NavDropdown>
+          </div>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
