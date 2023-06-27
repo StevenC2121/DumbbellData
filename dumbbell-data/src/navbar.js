1ca852fa2bfd927295956599ab842f78576fcfc2
StevenC2121 <stevencabral2121@gmail.com>
2023-06-27 13:45:56 -0400

    Updated navbar
    Gave it a cleaner look without the dark background and updated the code so it is consistent with the bootstrap 5 conventions.

--- dumbbell-data/src/navbar.js
+++ dumbbell-data/src/navbar.js
@@ -1,26 +1,40 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
 import { NavLink } from 'react-router-dom';
 
 const NavigationBar = () => {
   return (
-    <Navbar bg="dark" variant="dark" expand="lg">
-      <Navbar.Brand as={NavLink} to="/">Dumbbell Data</Navbar.Brand>
-      <Navbar.Toggle aria-controls="navbarNavDropdown" />
-      <Navbar.Collapse id="navbarNavDropdown">
-        <Nav className="mx-auto">
-          <div className="d-flex align-items-center justify-content-center">
-            <Nav.Link as={NavLink} to="/weightlifting" exact activeClassName="active">Weightlifting</Nav.Link>
-            <Nav.Link as={NavLink} to="/cardio" activeClassName="active">Cardio</Nav.Link>
-            <NavDropdown title="Graphs/Stats" id="navbarDropdownMenuLink">
-              <NavDropdown.Item as={NavLink} to="/wl-stats" activeClassName="active">Weightlifting</NavDropdown.Item>
-              <NavDropdown.Item as={NavLink} to="/cardio-stats" activeClassName="active">Cardio</NavDropdown.Item>
-            </NavDropdown>
-          </div>
-        </Nav>
-      </Navbar.Collapse>
-    </Navbar>
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="container-fluid">
+        <NavLink className="navbar-brand" to="/">Dumbbell Data</NavLink>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <NavLink className="nav-link" activeClassName="active" to="/weightlifting">Weightlifting</NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link" activeClassName="active" to="/cardio">Cardio</NavLink>
+            </li>
+            <li className="nav-item dropdown">
+              <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Graphs/Stats
+              </a>
+              <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                <li><NavLink className="dropdown-item" activeClassName="active" to="/wl-stats">Weightlifting</NavLink></li>
+                <li><NavLink className="dropdown-item" activeClassName="active" to="/cardio-stats">Cardio</NavLink></li>
+              </ul>
+            </li>
+          </ul>
+          <form className="d-flex">
+            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+            <button className="btn btn-outline-success" type="submit">Search</button>
+          </form>
+        </div>
+      </div>
+    </nav>
   );
 };
 
