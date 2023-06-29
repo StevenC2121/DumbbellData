611d2ac3a7b7bfc49a755c88f3cac5d710914bcc
StevenC2121 <stevencabral2121@gmail.com>
2023-06-29 16:40:16 -0400

    Exercise list created
    Beginning of the exercise list page creation. Most of the backend work of getting the list of exercises has been handled just have to finish the frontend section

--- dumbbell-data/src/pages/home.js
+++ dumbbell-data/src/pages/home.js
@@ -1,3 +1,51 @@
-export default function Home() {
-    return <p>Home</p>
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+import Exercise from '../models/exercise.model';
+
+export default class Home extends React.Component {
+  constructor(props) {
+    super(props);
+    
+    this.deleteExercise = this.deleteExercise.bind(this)
+    
+    this.state = {exercises: []};
+  } 
+  
+  componentDidMount() {
+    axios.get('http://localhost:5000/exercises/')
+      .then(response => {
+        this.setState({ exercises: response.data })
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+  }
+
+  deleteExercise(id) {
+    axios.delete('http://localhost:5000/exercises/'+id)
+      .then(res => console.log(res.data));
+    this.setState({
+      exercises: this.state.exercises.filter(el => el._id !== id)
+    })
+  }
+
+  exerciseList() {
+    return this.state.exercises.map(currentexercise => {
+      return <Exercise exercise={currentexercise} deleteExercise={this.deleteExercise} key={currentexercise._id} />;
+    })
+  }
+
+  render() {
+      return (
+        <div>
+          <h3>Logged Exercsises</h3>
+          <table className='table'>
+            <thread className='thread-light'>
+
+            </thread>
+          </table>
+        </div>
+      )
+    }
   }
