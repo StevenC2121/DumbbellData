449ee8d776ac3614f6df93304871d28190d0ef9c
StevenC2121 <stevencabral2121@gmail.com>
2023-07-08 13:12:16 -0400

    Updated navbar styling
    Makes use of justify-content-center and mx-2 tags to space out and center the navbar items for easier usability.

--- dumbbell-data/src/navbar.js
+++ dumbbell-data/src/navbar.js
@@ -7,19 +7,19 @@ const NavigationBar = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
       <div className="container-fluid">
-      <img src="/dbdata-logo.png" alt="" width="43.75" height="35" className="d-inline-block align-text-top"></img>
+        <img src="/dbdata-logo.png" alt="" width="43.75" height="35" className="d-inline-block align-text-top"></img>
         <NavLink className="navbar-brand" to="/">Dumbbell Data</NavLink>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav mx-auto">
+        <div className="collapse navbar-collapse justify-content-center" id="navbarSupportedContent">
+          <ul className="navbar-nav">
             <li className="nav-item">
-              <NavLink className="nav-link" to="/weightlifting">Weightlifting</NavLink>
+              <NavLink className="nav-link mx-2" to="/weightlifting">Weightlifting</NavLink>
             </li>
             <li className="nav-item">
               <div className="dropdown">
-                <NavLink className="nav-link dropdown-toggle" to="/graphs-stats" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                <NavLink className="nav-link dropdown-toggle mx-2" to="/graphs-stats" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                   Graphs/Stats
                 </NavLink>
                 <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
@@ -29,17 +29,15 @@ const NavigationBar = () => {
               </div>
             </li>
             <li className="nav-item">
-              <NavLink className="nav-link" to="/new-user">Account</NavLink>
+              <NavLink className="nav-link mx-2" to="/new-user">Account</NavLink>
             </li>            
           </ul>
-          <ul className="navbar-nav">
-            <li className="nav-item">
-              <div className="input-group">
-                <input className="form-control" type="search" placeholder="Search" aria-label="Search" />
-                <button className="btn btn-outline-success" type="submit">Search</button>
-              </div>
-            </li>
-          </ul>
+        </div>
+        <div className="d-flex">
+          <div className="input-group">
+            <input className="form-control" type="search" placeholder="Search" aria-label="Search" />
+            <button className="btn btn-outline-success" type="submit">Search</button>
+          </div>
         </div>
       </div>
     </nav>
