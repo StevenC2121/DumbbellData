83feca1642eea0895a8ca76dc1a49878e840a7ac
StevenC2121 <stevencabral2121@gmail.com>
2023-07-10 12:48:35 -0400

    Update home page
    Uses useState and useEffect for exercise component. working on setting up for the login page.

--- dumbbell-data/src/pages/home.js
+++ dumbbell-data/src/pages/home.js
@@ -1,86 +1,105 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import UserService from "../services/user.service";
 
-const Exercise = props => (
-  <tr>
-    <td>{props.exercise.username}</td>
-    <td>{props.exercise.description}</td>
-    <td>{props.exercise.duration}</td>
-    <td>{props.exercise.date.substring(0, 10)}</td>
-    <td>
-      <button
-        className="btn btn-link"
-        onClick={() => {
-          window.location.href = '/edit-exercise/' + props.exercise._id;
-        }}
-      >
-        edit
-      </button>
-      {' | '}
-      <button
-        className="btn btn-link"
-        onClick={() => {
-          props.deleteExercise(props.exercise._id);
-        }}
-      >
-        delete
-      </button>
-    </td>
-  </tr>
-);
+const Exercise = (props) => {
+  const [content, setContent] = useState("");
 
-export default class Home extends React.Component {
-  constructor(props) {
-    super(props);
-    
-    this.deleteExercise = this.deleteExercise.bind(this)
-    
-    this.state = {exercises: []};
-  } 
-  
-  componentDidMount() {
+  useEffect(() => {
+    UserService.getPublicContent().then(
+      (response) => {
+        setContent(response.data);
+      },
+      (error) => {
+        const _content =
+          (error.response && error.response.data) ||
+          error.message ||
+          error.toString();
+
+        setContent(_content);
+      }
+    );
+  }, []);
+
+  return (
+    <tr>
+      <td>{props.exercise.username}</td>
+      <td>{props.exercise.description}</td>
+      <td>{props.exercise.duration}</td>
+      <td>{props.exercise.date.substring(0, 10)}</td>
+      <td>
+        <button
+          className="btn btn-link"
+          onClick={() => {
+            window.location.href = '/edit-exercise/' + props.exercise._id;
+          }}
+        >
+          edit
+        </button>
+        {' | '}
+        <button
+          className="btn btn-link"
+          onClick={() => {
+            props.deleteExercise(props.exercise._id);
+          }}
+        >
+          delete
+        </button>
+      </td>
+    </tr>
+  );
+};
+
+const Home = () => {
+  const [exercises, setExercises] = useState([]);
+
+  const deleteExercise = (id) => {
+    axios.delete('http://localhost:5000/exercises/' + id)
+      .then(res => console.log(res.data));
+    setExercises(exercises.filter(el => el._id !== id));
+  };
+
+  useEffect(() => {
     axios.get('http://localhost:5000/exercises/')
       .then(response => {
-        this.setState({ exercises: response.data })
+        setExercises(response.data);
       })
       .catch((error) => {
         console.log(error);
-      })
-  }
+      });
+  }, []);
 
-  deleteExercise(id) {
-    axios.delete('http://localhost:5000/exercises/'+id)
-      .then(res => console.log(res.data));
-    this.setState({
-      exercises: this.state.exercises.filter(el => el._id !== id)
-    })
-  }
-
-  exerciseList() {
-    return this.state.exercises.map(currentexercise => {
-      return <Exercise exercise={currentexercise} deleteExercise={this.deleteExercise} key={currentexercise._id} />;
-    })
-  }
-
-  render() {
+  const exerciseList = () => {
+    return exercises.map(currentExercise => {
       return (
-        <div>
-          <h3>Logged Exercsises</h3>
-          <table className='table'>
-            <thead className='thread-light'>
-              <tr>
-                <th>Username</th>
-                <th>Lift Description</th>
-                <th>Weight (in lb)</th>
-                <th>Date</th>
-                <th>Actions</th>
-              </tr>
-            </thead>
-            <tbody>
-              { this.exerciseList() }
-            </tbody>
-          </table>
-        </div>
-      )
-    }
-  }
+        <Exercise
+          exercise={currentExercise}
+          deleteExercise={deleteExercise}
+          key={currentExercise._id}
+        />
+      );
+    });
+  };
+
+  return (
+    <div>
+      <h3>Logged Exercises</h3>
+      <table className='table'>
+        <thead className='thead-light'>
+          <tr>
+            <th>Username</th>
+            <th>Lift Description</th>
+            <th>Weight (in lb)</th>
+            <th>Date</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {exerciseList()}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default Home;
