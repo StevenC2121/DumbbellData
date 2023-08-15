f045159fbd9d23cdd56800a988fd55ec9f6a253c
StevenC2121 <stevencabral2121@gmail.com>
2023-08-15 12:59:25 -0400

    Error/warning fixes
    Instals crypto broswerify and updates index,js to be valid on react 18 using create root.

--- dumbbell-data/src/index.js
+++ dumbbell-data/src/index.js
@@ -1,15 +1,16 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client"; // Import createRoot
 import { BrowserRouter as Router } from "react-router-dom";
 import { UserProvider } from "./UserContext"; // Adjust the path
 
 import App from "./App"; // Your main app component
 
-ReactDOM.render(
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement); // Create a root
+root.render(
   <Router>
     <UserProvider>
       <App />
     </UserProvider>
-  </Router>,
-  document.getElementById("root")
+  </Router>
 );
