750b6c28418cc52bcaab03fd829e979fa33d81a0
StevenC2121 <stevencabral2121@gmail.com>
2023-08-15 14:24:29 -0400

    Home page changes
    Home page getting ready to display only active users weightlifting stats. More changes needed for it to be fully functional

--- dumbbell-data/src/index.js
+++ dumbbell-data/src/index.js
@@ -1,11 +1,11 @@
-import React from "react";
-import { createRoot } from "react-dom/client"; // Import createRoot
-import { BrowserRouter as Router } from "react-router-dom";
-import { UserProvider } from "./UserContext"; // Adjust the path
+import React from 'react';
+import { createRoot } from 'react-dom'; // Import createRoot from 'react-dom'
+import { BrowserRouter as Router } from 'react-router-dom';
+import { UserProvider } from './UserContext'; // Adjust the path
 
-import App from "./App"; // Your main app component
+import App from './App'; // Your main app component
 
-const rootElement = document.getElementById("root");
+const rootElement = document.getElementById('root');
 const root = createRoot(rootElement); // Create a root
 root.render(
   <Router>
--- dumbbell-data/src/pages/home.js
+++ dumbbell-data/src/pages/home.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import axios from 'axios';
+import { useUser } from '../UserContext';
 
 const Exercise = props => (
   <tr>
@@ -29,58 +30,59 @@ const Exercise = props => (
   </tr>
 );
 
-export default class Home extends React.Component {
-  constructor(props) {
-    super(props);
-    
-    this.deleteExercise = this.deleteExercise.bind(this)
-    
-    this.state = {exercises: []};
-  } 
-  
-  componentDidMount() {
-    axios.get('http://localhost:5000/exercises/')
-      .then(response => {
-        this.setState({ exercises: response.data })
+const Home = () => {
+  const user = useUser(); // Call useUser hook to get the user from context
+
+  const [exercises, setExercises] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:5000/exercises/')
+      .then((response) => {
+        const userExercises = response.data.filter(
+          (exercise) => exercise.username === user.currentUser // Use user.currentUser
+        );
+        setExercises(userExercises);
       })
       .catch((error) => {
         console.log(error);
-      })
-  }
+      });
+  }, [user.currentUser]); // Add user.currentUser as a dependency
 
-  deleteExercise(id) {
-    axios.delete('http://localhost:5000/exercises/'+id)
-      .then(res => console.log(res.data));
-    this.setState({
-      exercises: this.state.exercises.filter(el => el._id !== id)
-    })
-  }
+  const deleteExercise = (id) => {
+    axios
+      .delete(`http://localhost:5000/exercises/${id}`)
+      .then((res) => console.log(res.data));
+    setExercises((prevExercises) => prevExercises.filter((el) => el._id !== id));
+  };
 
-  exerciseList() {
-    return this.state.exercises.map(currentexercise => {
-      return <Exercise exercise={currentexercise} deleteExercise={this.deleteExercise} key={currentexercise._id} />;
-    })
-  }
+  const exerciseList = () => {
+    return exercises.map((currentexercise) => (
+      <Exercise
+        exercise={currentexercise}
+        deleteExercise={deleteExercise}
+        key={currentexercise._id}
+      />
+    ));
+  };
 
-  render() {
-      return (
-        <div>
-          <h3>Logged Exercsises</h3>
-          <table className='table'>
-            <thead className='thread-light'>
-              <tr>
-                <th>Username</th>
-                <th>Lift Description</th>
-                <th>Weight (in lb)</th>
-                <th>Date</th>
-                <th>Actions</th>
-              </tr>
-            </thead>
-            <tbody>
-              { this.exerciseList() }
-            </tbody>
-          </table>
-        </div>
-      )
-    }
-  }
+  return (
+    <div>
+      <h3>Logged Exercises</h3>
+      <table className='table'>
+        <thead className='thread-light'>
+          <tr>
+            <th>Username</th>
+            <th>Lift Description</th>
+            <th>Weight (in lb)</th>
+            <th>Date</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>{exerciseList()}</tbody>
+      </table>
+    </div>
+  );
+};
+
+export default Home;
