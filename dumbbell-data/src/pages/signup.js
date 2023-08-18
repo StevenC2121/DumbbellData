67ec746d3432658a72c1adae59005fd272d0ccb9
StevenC2121 <stevencabral2121@gmail.com>
2023-08-18 11:43:24 -0400

    Update signup.js
    Uses react hooks to set the active user so other parts of the program know who the user is upon signup.

--- dumbbell-data/src/pages/signup.js
+++ dumbbell-data/src/pages/signup.js
@@ -1,43 +1,61 @@
-import React, {useEffect, useState} from "react"
-import axios from "axios"
-import bcrypt from "bcryptjs"
-import { useNavigate, Link } from "react-router-dom"
+import React, { useState } from "react";
+import axios from "axios";
+import { useNavigate, Link } from "react-router-dom";
+import bcrypt from "bcryptjs";
+import { useUser } from "../UserContext"; // Import useUser from UserContext
 
 const Signup = () => {
-    const [email, setEmail]=useState('')
-    const [password, setPassword]=useState('')
+  const history = useNavigate();
+  const { setCurrentUser } = useUser(); // Get the setCurrentUser function from the user context
 
-    async function submit(e) {
-        e.preventDefault();
-      
-        try {
-          const saltRounds = 10; 
-       
-          const hashedPassword = await bcrypt.hash(password, saltRounds);
-      
-          await axios.post("http://localhost:5000/users/add", {
-            email,
-            password: hashedPassword, 
-          });
-        } catch (e) {
-          console.log(e);
-        }
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  async function submit(e) {
+    e.preventDefault();
+
+    try {
+      const saltRounds = 10;
+      const hashedPassword = await bcrypt.hash(password, saltRounds);
+
+      const response = await axios.post("http://localhost:5000/users/add", {
+        email,
+        password: hashedPassword,
+      });
+
+      if (response.status === 200) {
+        setCurrentUser(email); // Set the current user in the context
+        history("/home"); // Redirect to home page after successful signup
       }
+    } catch (e) {
+      console.log(e);
+    }
+  }
 
-    return (
-        <div>
-            <h1>Signup</h1>
+  return (
+    <div>
+      <h1>Signup</h1>
 
-            <form action="POST">
-                <input type="email" onChange={(e)=>{setEmail(e.target.value)}} placeholder="email" />
-                <input type="password" onChange={(e)=>{setPassword(e.target.value)}} placeholder="password" />
-                <input type="submit" onClick={submit} />
-            </form>
-            <br />
-            <p>OR</p>
-            <Link to="/">Login Page</Link>
-        </div>
-    )
-}
+      <form action="POST" onSubmit={submit}>
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="email"
+        />
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="password"
+        />
+        <input type="submit" />
+      </form>
+      <br />
+      <p>OR</p>
+      <Link to="/">Login Page</Link>
+    </div>
+  );
+};
 
-export default Signup
+export default Signup;
