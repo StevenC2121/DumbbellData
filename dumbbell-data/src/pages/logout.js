ecc5ef3b07fe5cc032cc35d25726d8c30c319aa2
StevenC2121 <stevencabral2121@gmail.com>
2023-08-23 13:19:38 -0400

    Update logout.js
    Logout now displays a message upon successfully logging out of an account. After a slight delay to view the message the user is then redirected back to the login page.

--- dumbbell-data/src/pages/logout.js
+++ dumbbell-data/src/pages/logout.js
@@ -1,21 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useUser } from "../UserContext";
 
 const Logout = () => {
   const navigate = useNavigate();
   const { setCurrentUser } = useUser();
+  const [logoutMessage, setLogoutMessage] = useState(null);
 
   const handleLogout = () => {
     // Clear any user data from context
     setCurrentUser(null);
 
-    // Redirect to the login page
-    navigate("/");
+    // Display logout message
+    setLogoutMessage("Logout successful! Redirecting...");
+
+    // Redirect to the login page after a brief delay
+    setTimeout(() => {
+      navigate("/");
+    }, 1000); 
   };
 
   return (
     <div>
+      {logoutMessage && <p>{logoutMessage}</p>}
       <button onClick={handleLogout}>Logout</button>
     </div>
   );
