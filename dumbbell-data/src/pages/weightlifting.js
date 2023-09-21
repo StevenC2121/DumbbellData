07115232260d4cc4c6c46cd713c13255ad1a79b6
Steven Cabral <stevencabral2121@gmail.com>
2023-09-21 12:58:44 -0400

    Updated logging page
    Logging page now includes png image and a rounded box around the input fields and submit box. Updates include image file, custom CSS, and small updates to the logging page.

--- dumbbell-data/src/pages/weightlifting.js
+++ dumbbell-data/src/pages/weightlifting.js
@@ -3,11 +3,12 @@ import axios from 'axios';
 import DatePicker from 'react-datepicker';
 import "react-datepicker/dist/react-datepicker.css";
 import { useUser } from '../UserContext';
-import { useNavigate } from 'react-router-dom'; // Use useNavigate instead of useHistory
+import { useNavigate } from 'react-router-dom';
+import './weightlifting.css';
 
 export default function Weightlifting() {
   const user = useUser();
-  const navigate = useNavigate(); // Get the navigate function
+  const navigate = useNavigate();
 
   const [description, setDescription] = React.useState('');
   const [duration, setDuration] = React.useState(0);
@@ -29,54 +30,56 @@ export default function Weightlifting() {
       const response = await axios.post('http://localhost:5000/exercises/add', exercise);
       console.log(response.data);
 
-      // Reset form
       setDescription('');
       setDuration(0);
       setDate(new Date());
 
-      // Use navigate to go to the home page
       navigate('/home');
     } catch (error) {
       console.error(error);
     }
   }
+  
   return (
-    <div>
-      <h3>Create New Weightlifting Log</h3>
-      <form onSubmit={handleSubmit}>
-        <div className='form-group'>
-          {/* Email is automatically filled from the currently logged-in user */}
-          <label>Lift Description: </label>
-          <input 
-            type='text'
-            className='form-control'
-            value={description}
-            onChange={(e) => setDescription(e.target.value)}
-          />
-        </div>
-        <div className='form-group'>
-          <label>Weight (in lb): </label>
-          <input
-            type='text'
-            className='form-control'
-            value={duration}
-            onChange={(e) => setDuration(e.target.value)}
-          />          
-        </div>
-        <div className='form-group'>
-          <label>Date: </label>
-          <div>
-            <DatePicker
-              selected={date}
-              onChange={setDate}
+    <div className="exercise-container">
+      <div className="exercise-box">
+        <h3>Create New Weightlifting Log</h3>
+        <form onSubmit={handleSubmit} className='exercise-form'>
+          <div className='form-group'>
+            <label>Lift Description: </label>
+            <input 
+              type='text'
+              className='form-control'
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
             />
           </div>
-        </div>
-        
-        <div className='form-group'>
-          <input type='submit' value='Create Exercise Log' className='btn btn-primary' />
-        </div>
-      </form>
+          <div className='form-group'>
+            <label>Weight (in lb): </label>
+            <input
+              type='text'
+              className='form-control'
+              value={duration}
+              onChange={(e) => setDuration(e.target.value)}
+            />          
+          </div>
+          <div className='form-group'>
+            <label>Date: </label>
+            <div>
+              <DatePicker
+                selected={date}
+                onChange={setDate}
+              />
+            </div>
+          </div>
+          <div className='form-group'>
+            <input type='submit' value='Create Exercise Log' className='btn btn-primary' />
+          </div>
+        </form>
+      </div>
+      <div className="exercise-image">
+        <img src="/barbellcurl.png" alt="Barbell Curl" width={300} height={300}/>
+      </div>
     </div>
   );
 }
