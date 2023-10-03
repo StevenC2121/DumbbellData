0783a49c3d09dfb086a15d8619eb6057abdc547f
Steven Cabral <stevencabral2121@gmail.com>
2023-10-03 18:27:06 -0400

    Prepare for deployment
    Patch prepares for deployment on render.com

--- dumbbell-data/src/index.js
+++ dumbbell-data/src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { UserProvider } from './UserContext';
 import App from './App';
+import { disableReactDevTools } from '@fvilers/disable-react-devtools';
+
+if(process.env.NODE_ENV === 'production') disableReactDevTools()
 
 const rootElement = document.getElementById('root');
 const root = ReactDOM.createRoot(rootElement);
